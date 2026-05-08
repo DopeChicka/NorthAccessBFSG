1d69cd05d862b6cf376980b962435b2d4e0feafa
NorthAccess <y.channelasap@gmail.com>
2026-05-08 17:31:11 +0200

    Wire discovery models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,14 +1,19 @@
 from app.models.compliance_finding import ComplianceFinding
+from app.models.discovery_run import DiscoveryRun, DiscoveryRunStatus
 from app.models.evidence_bundle import EvidenceBundle
 from app.models.finding import Finding
 from app.models.lead import Lead
+from app.models.lead_candidate import LeadCandidate
 from app.models.scan import Scan, ScanStatus
 
 __all__ = [
     "ComplianceFinding",
+    "DiscoveryRun",
+    "DiscoveryRunStatus",
     "EvidenceBundle",
     "Finding",
     "Lead",
+    "LeadCandidate",
     "Scan",
     "ScanStatus",
 ]
