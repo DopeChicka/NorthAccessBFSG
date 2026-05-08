76f852cb35d06b11e3b0c253278ef5dfea6ff728
NorthAccess <y.channelasap@gmail.com>
2026-05-08 13:59:18 +0200

    Export evidence bundle model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,14 @@
 from app.models.compliance_finding import ComplianceFinding
+from app.models.evidence_bundle import EvidenceBundle
 from app.models.finding import Finding
 from app.models.lead import Lead
 from app.models.scan import Scan, ScanStatus
 
-__all__ = ["ComplianceFinding", "Finding", "Lead", "Scan", "ScanStatus"]
+__all__ = [
+    "ComplianceFinding",
+    "EvidenceBundle",
+    "Finding",
+    "Lead",
+    "Scan",
+    "ScanStatus",
+]
