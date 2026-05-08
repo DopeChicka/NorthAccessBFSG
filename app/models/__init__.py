10be9c25a976699d6686da1a7725f7215f5e6a95
NorthAccess <y.channelasap@gmail.com>
2026-05-08 13:42:58 +0200

    Export compliance finding model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
+from app.models.compliance_finding import ComplianceFinding
 from app.models.finding import Finding
 from app.models.lead import Lead
 from app.models.scan import Scan, ScanStatus
 
-__all__ = ["Finding", "Lead", "Scan", "ScanStatus"]
+__all__ = ["ComplianceFinding", "Finding", "Lead", "Scan", "ScanStatus"]
